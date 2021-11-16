991f16e915dbb9e31aa4f48137c85032fe21d17a
thucanh5871 <94441948+thucanh5871@users.noreply.github.com>
2021-11-16 16:49:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ const CONFIG = {
     introTitle: 'Alu Sư Tử Gọi Song Ngư',
     introDesc: `Thế giới này hơn 7 tỉ người nhưng sao mình lại hợp nhau đến thế 🖤`,
     btnIntro: 'Bấm vào đây',
-    title: 'MY LOVE FOR YOU',
+    title: 'Hello pé iu',
     desc: 'Chúng mình mãi mãi bên nhau có được hông 🥺',
     btnYes: 'Đương nhiên rồi 😍',
     btnNo: 'Hông, ai gảnh 😒',
     question: 'Tại sao mình lại chơi lâu thế nhờ?',
     btnReply: 'Trả lời',
-    reply: 'Tại Trúc dễ thương quá mà 😜',
+    reply: 'Tại Trúc dễ thương quá mà ><',
     mess: 'Hoi giỡn thôi, pé mới là đáng iu nhất 🥰',
     messDesc: 'Khi nào rảnh thì mình đi chơi chung nha 😘',
     btnAccept: 'Ok na khaaaa ><',
